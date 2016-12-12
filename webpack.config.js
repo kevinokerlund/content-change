19cbba3e1c9fdd8ef968f70cffda5d61bf203365
Kevin Okerlund <kevin.m.okerlund@gmail.com>
2016-12-12 11:45:24 -0700

    Add .gitignore, package.json, and webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+	entry: './src/content-change.js',
+	devtool: 'source-map',
+	output: {
+		path: './lib',
+		filename: 'content-change.js',
+		library: 'ContentChange',
+		libraryTarget: 'umd',
+		umdNamedDefine: true
+	},
+	module: {
+		loaders: [
+			{
+				loader: 'babel',
+				test: /\.js$/,
+				exclude: /node_modules/,
+				query: {
+					presets: ['es2015'],
+					plugins: ['babel-plugin-add-module-exports']
+				}
+			}
+		]
+	},
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false,
+			},
+			output: {
+				comments: false,
+			},
+		}),
+	]
+};
